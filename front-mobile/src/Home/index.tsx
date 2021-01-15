8f3115ebe0c065fdcd760e65a3cbc1c941735845
Charles Vaghetti <ch2vaghetti@gmail.comgit config --global user.emailgit config --global user.email ch2vaghetti@gmail.comgit config --global user.email ch2vaghetti@gmail.com>
2021-01-14 21:44:16 -0300

    Add Header front-mobile

---
+++ front-mobile/src/Home/index.tsx
@@ -0,0 +1,17 @@
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+function Home() {
+   return (
+    <View>
+
+        <Text>Conteudo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Home;
